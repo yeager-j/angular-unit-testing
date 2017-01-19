e504bbe10515483e425368e9bfc8af4ab9e097d1
Jackson Yeager <Jackson Yeager>
2017-01-19 14:31:45 -0500

    Added unit testing with API calls

--- app/app.js
+++ app/app.js
@@ -5,8 +5,43 @@
 angular.module('unit-testing', [])
     .controller('UnitTestController', UnitTestController);
 
-UnitTestController.$inject = ['$rootScope'];
-function UnitTestController($rootScope) {
+UnitTestController.$inject = ['$http'];
+function UnitTestController($http) {
     var vm = this;
-    vm.hello = 'Hello, World!';
+
+    vm.title = 'Angular Unit Testing';
+    vm.apiKey = 'c9f1140b11ead40a9c2b78dfe2c61776';
+    vm.destinations = [];
+    vm.newDestination = {
+        city: undefined,
+        country: undefined
+    };
+
+    vm.addDestination = function () {
+        vm.destinations.push({
+            city: vm.newDestination.city,
+            country: vm.newDestination.country
+        });
+    };
+
+    vm.removeDestination = function (index) {
+        vm.destinations.splice(index, 1);
+    };
+
+    vm.getWeather = function (destination) {
+        $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + destination.city + '&appid=' + vm.apiKey)
+            .then(function success(response) {
+                if (response.data.weather) {
+                    destination.weather = {};
+                    destination.weather.main = response.data.weather[0].main;
+                    destination.weather.temp = kelvinToCelsius(response.data.main.temp);
+                }
+            }, function failure(response) {
+                vm.title += ' - Error fetching weather data. Error: ' + response;
+            })
+    };
+
+    function kelvinToCelsius(temp) {
+        return Math.round(temp - 273);
+    }
 }
--- app/test/unit/main.js
+++ app/test/unit/main.js
@@ -6,19 +6,89 @@ describe('Unit Testing', function () {
     beforeEach(module('unit-testing'));
 
     describe('Main Controller', function () {
-        var ctrl;
+        var ctrl, httpBackend;
 
-        beforeEach(inject(function ($controller) {
+        beforeEach(inject(function ($controller, $httpBackend) {
             ctrl = $controller('UnitTestController', {});
+            httpBackend = $httpBackend;
         }));
 
         afterEach(function () {
-            // Cleanup code
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
         });
 
-        it('should init a variable called "hello"', function () {
-            expect(ctrl.hello).toBeDefined();
-            expect(ctrl.hello).toBe('Hello, World!');
+        it('should init a variable called "title"', function () {
+            expect(ctrl.title).toBeDefined();
+            expect(ctrl.title).toBe('Angular Unit Testing');
+        });
+
+        it('should add 2 destinations to the list', function () {
+            expect(ctrl.destinations).toBeDefined();
+            expect(ctrl.destinations.length).toBe(0);
+
+            ctrl.newDestination = {
+                city: "London",
+                country: "England"
+            };
+
+            ctrl.addDestination();
+
+            expect(ctrl.destinations.length).toBe(1);
+            expect(ctrl.destinations[0].city).toBe('London');
+            expect(ctrl.destinations[0].country).toBe('England');
+
+            ctrl.newDestination = {
+                city: "Athens",
+                country: "Greece"
+            };
+
+            ctrl.addDestination();
+
+            expect(ctrl.destinations.length).toBe(2);
+            expect(ctrl.destinations[0].city).toBe('London');
+            expect(ctrl.destinations[0].country).toBe('England');
+            expect(ctrl.destinations[1].city).toBe('Athens');
+            expect(ctrl.destinations[1].country).toBe('Greece');
+        });
+
+        it('should remove a destination from the destinations list', function () {
+            ctrl.destinations = [
+                {
+                    city: "London",
+                    country: "England"
+                },
+                {
+                    city: "Paris",
+                    country: "France"
+                }
+            ];
+
+            expect(ctrl.destinations).toBeDefined();
+            expect(ctrl.destinations.length).toBe(2);
+
+            ctrl.removeDestination(0);
+
+            expect(ctrl.destinations.length).toBe(1);
+            expect(ctrl.destinations[0].city).toBe("Paris");
+        });
+
+        it('should update the weather for a specific destination', function () {
+            ctrl.destination = {
+                city: 'Atlanta',
+                country: 'USA'
+            };
+
+            httpBackend.expectGET('http://api.openweathermap.org/data/2.5/weather?q=' + ctrl.destination.city + '&appid=' + ctrl.apiKey)
+                .respond({
+                    weather: [{main: 'Rain', detail: 'Light rain'}],
+                    main: {temp: 288}
+                });
+            ctrl.getWeather(ctrl.destination);
+            httpBackend.flush();
+
+            expect(ctrl.destination.weather.main).toBe('Rain');
+            expect(ctrl.destination.weather.temp).toBe(15);
         })
     })
 });
